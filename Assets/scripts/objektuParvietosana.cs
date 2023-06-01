5b9ec17dfe458e5c34378ea64a077b8ddb345592
Dzonatans <Dzonatan2017@gmail.com>
2023-06-01 08:58:03 +0300

    izmers

--- Assets/scripts/objektuParvietosana.cs
+++ Assets/scripts/objektuParvietosana.cs
@@ -19,6 +19,39 @@ public class objektuParvietosana : MonoBehaviour {
             {
                 objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, -Time.deltaTime * 15f);
             }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+				if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y <= 1.50f) {
+					objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale =
+						new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y + 0.005f);
+                }
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y >= 0.45f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y - 0.005f);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x <= 1.50f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x + 0.005f, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y );
+                }
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x >= 0.45f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x- 0.005f, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y) ;
+                }
+            }
+
         }
 	}
 }
