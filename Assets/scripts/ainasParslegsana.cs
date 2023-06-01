d2c0bd423abcb737d36d80048c00b6a87f58d2df
Dzonatans <Dzonatan2017@gmail.com>
2023-06-01 09:45:07 +0300

    scenas Parslegsana

---
+++ Assets/scripts/ainasParslegsana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ainasParslegsana : MonoBehaviour {
+
+    public void apturet()
+    {
+        Application.Quit();
+    }
+
+    public void uzSceneSpelet()
+    {
+        SceneManager.LoadScene("Pilseta", LoadSceneMode.Single);
+    }
+    public void uzSceneSakums()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+}
+
