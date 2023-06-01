0ce61db86a8d82a85c894e962ca4e9e5cb02afa2
Dzonatans <Dzonatan2017@gmail.com>
2023-06-01 08:26:25 +0300

    rotarion

---
+++ Assets/scripts/objektuParvietosana.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class objektuParvietosana : MonoBehaviour {
+	public objekti objektuSkripts;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(objektuSkripts.pedejaisVilktais != null)
+		{
+			if(Input.GetKey(KeyCode.Z))
+			{
+				objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0,0, Time.deltaTime * 15f);
+			}
+            if (Input.GetKey(KeyCode.X))
+            {
+                objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, -Time.deltaTime * 15f);
+            }
+        }
+	}
+}
